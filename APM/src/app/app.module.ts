16c5dd1bc708d2a813d1630aa2a3fb16ff2a6d79
BarasaK <nicholas.k.barasa@gmail.com>
2021-10-13 17:08:15 +0300

    navigation and routing

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -4,21 +4,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './product/product-list.component';
+import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipes';
 import { StarComponent } from './shared/star-component';
+import { ProductDetailComponent } from './products/product-detail.component';
+import { RouterModule } from '@angular/router';
+import { WelcomeComponent } from './home/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ConvertToSpacesPipe,
-    StarComponent
+    StarComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'products', component: ProductListComponent},
+      {path: 'products/:id', component: ProductDetailComponent},
+      {path: 'welcome', component: WelcomeComponent},
+      {path: '', redirectTo: 'welcome',pathMatch: 'full'},
+      {path: '', redirectTo: 'welcome',pathMatch: 'full'}
+
+    ])
   ],
   bootstrap: [AppComponent]
 })
